2075cbf961db273ecc54ddd21fcc2fe354971056
Guilherme R <grsguiga@gmail.com>
2022-04-08 19:07:43 -0300

    Requisito 2

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Login from './pages/Login';
+import Wallet from './pages/Wallet';
 import './App.css';
 
 function App() {
   return (
     <div>
       <Switch>
+        <Route path="/carteira" component={ Wallet } />
         <Route path="/" component={ Login } />
       </Switch>
     </div>
